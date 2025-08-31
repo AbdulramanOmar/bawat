cfc3171744fa1ad473601d2eeaa6fb02aed1357c
AbdulramanOmar <168247524+AbdulramanOmar@users.noreply.github.com>
2025-08-31 15:18:47 +0300

    Update page.jsx

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,18 +3,20 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ProjectsSection from "../sections/ProjectsSection";
 import BasieHero from "../sections/BasieHero";
+import MobileProjectsSection from "../sections/MobileProjectsSection";
 
 const page = () => {
   return (
     <main className="bg-[#394854]">
       <Navbar />
         <div  className='pt-50'/>
-
       <BasieHero title="Projects" />
       <ProjectsSection />
+      <MobileProjectsSection />
       <Footer />
     </main>
   );
 };
 
 export default page;
+
